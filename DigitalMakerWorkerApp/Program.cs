7690822aa385d72015e2a22d62d735d01bdcd2e6
SensibleHoneybee <sensible.honeybee@yahoo.com>
2022-08-21 16:01:21 +0100

    Add URL parsing to arguments

--- DigitalMakerWorkerApp/Program.cs
+++ DigitalMakerWorkerApp/Program.cs
@@ -2,7 +2,32 @@ using DigitalMakerWorkerApp;
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Logging.EventLog;
 
-using IHost host = Host.CreateDefaultBuilder(args)
+var argCounter = 0;
+string? url = null;
+while (argCounter < args.Length)
+{
+    var nextArg = args[argCounter++];
+    if (nextArg != null && nextArg == "-url")
+    {
+        if (argCounter < args.Length)
+        {
+            url = args[argCounter++];
+            continue;
+        }
+    }
+
+    // If we get here, an unknown parameter or missing URL etc. was detected
+    Console.WriteLine("Invalid arguments.\r\nUsage: DigitalMakerWorkerApp.exe -url <URL>");
+    return;
+}
+
+if (url == null)
+{
+    Console.WriteLine("You must specify a URL.\r\nUsage: DigitalMakerWorkerApp.exe -url <URL>");
+    return;
+}
+
+using IHost host = Host.CreateDefaultBuilder()
     .UseWindowsService(options =>
     {
         options.ServiceName = "DigitalMaker Worker App Service";
