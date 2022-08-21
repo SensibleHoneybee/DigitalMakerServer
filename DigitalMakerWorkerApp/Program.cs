284ddb0e36700b74728804d771984539e79bb5d2
SensibleHoneybee <sensible.honeybee@yahoo.com>
2022-08-21 16:18:16 +0100

    add launchSettings.json back and move secrets to secrets.json

--- DigitalMakerWorkerApp/Program.cs
+++ DigitalMakerWorkerApp/Program.cs
@@ -2,32 +2,7 @@ using DigitalMakerWorkerApp;
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Logging.EventLog;
 
-var argCounter = 0;
-string? url = null;
-while (argCounter < args.Length)
-{
-    var nextArg = args[argCounter++];
-    if (nextArg != null && nextArg == "-url")
-    {
-        if (argCounter < args.Length)
-        {
-            url = args[argCounter++];
-            continue;
-        }
-    }
-
-    // If we get here, an unknown parameter or missing URL etc. was detected
-    Console.WriteLine("Invalid arguments.\r\nUsage: DigitalMakerWorkerApp.exe -url <URL>");
-    return;
-}
-
-if (url == null)
-{
-    Console.WriteLine("You must specify a URL.\r\nUsage: DigitalMakerWorkerApp.exe -url <URL>");
-    return;
-}
-
-using IHost host = Host.CreateDefaultBuilder()
+using IHost host = Host.CreateDefaultBuilder(args)
     .UseWindowsService(options =>
     {
         options.ServiceName = "DigitalMaker Worker App Service";
