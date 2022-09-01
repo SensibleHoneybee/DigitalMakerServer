f4834ebaba28a392bfe782893b81f201518bda84
SensibleHoneybee <sensible.honeybee@yahoo.com>
2022-09-01 20:55:13 +0100

    add barcode scanned request

---
+++ DigitalMakerApi/Requests/BarcodeScannedRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DigitalMakerApi.Requests
+{
+    public class BarcodeScannedRequest
+    {
+        public string InstanceId { get; set; } = string.Empty;
+
+        public string InstanceName { get; set; } = string.Empty;
+    }
+}
